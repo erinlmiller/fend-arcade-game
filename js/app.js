057e21d9ae3a539001cf41ab427291dbfe5630a6
Erin Miller <erinthered324@gmail.com>
2018-07-14 15:48:03 -0400

    Add enemies and enemy movement

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
+    this.step = 101;
+    this.x = x;
+    this.y = y + 55;
+    this.boundary = this.step * 5;
+    this.resetPos = -this.step;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +15,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < this.boundary) {
+      this.x += this.speed * dt;
+    }
+    else {
+      this.x = this.resetPos;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -63,13 +70,17 @@ class Hero {
   }
 }
 
-const player = new Hero();
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+const player = new Hero();
+const bug1 = new Enemy(-101, 0, 200);
+const bug2 = new Enemy(-101, 83, 300);
+const bug3 = new Enemy((-101*2.5), (83*2), 350);
+const allEnemies = [];
+allEnemies.push(bug1,bug2,bug3);
+console.log(allEnemies);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
