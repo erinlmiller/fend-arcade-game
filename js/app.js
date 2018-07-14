8e57eeb12ef83059a136b36bc4211c577978b2a8
Erin Miller <erinthered324@gmail.com>
2018-07-14 15:11:28 -0400

    Make board and player show up

--- js/app.js
+++ js/app.js
@@ -24,7 +24,18 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Hero {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.sprite = 'images/char-boy.png';
+  }
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
 
+const player = new Hero();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
